a2085dfd1aa762b469d173c0bd4c4c15e59ca5b0
Degamisu <degamisu@gmail.com>
2023-11-11 12:34:54 -0500

    bug fixes

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -6,13 +6,11 @@ namespace AVSP
 {
     public partial class MainWindow : Window
     {
-        // Store the selected image path
-        private string selectedImagePath;
+        private string? selectedImagePath;
 
         public MainWindow()
         {
             InitializeComponent();
-            // Add any other initialization logic here
         }
 
         private void OpenButton_Click(object sender, RoutedEventArgs e)
@@ -21,23 +19,18 @@ namespace AVSP
 
             if (openFileDialog.ShowDialog() == true)
             {
-                // Store the selected image path
                 selectedImagePath = openFileDialog.FileName;
-
-                // Display the selected image
                 DisplayImage(selectedImagePath);
             }
         }
 
         private void ExitButton_Click(object sender, RoutedEventArgs e)
         {
-            // Close the application
             Close();
         }
 
         private void OpenImageViewer_Click(object sender, RoutedEventArgs e)
         {
-            // Open the image viewer window and pass the stored image path
             if (!string.IsNullOrEmpty(selectedImagePath))
             {
                 ImageViewerWindow imageViewer = new ImageViewerWindow(selectedImagePath);
@@ -51,7 +44,6 @@ namespace AVSP
 
         private void OpenOscilloscope_Click(object sender, RoutedEventArgs e)
         {
-            // Open the OscilloscopeWindow and pass the stored image path
             if (!string.IsNullOrEmpty(selectedImagePath))
             {
                 OscilloscopeWindow oscilloscopeWindow = new OscilloscopeWindow(selectedImagePath);
@@ -67,14 +59,11 @@ namespace AVSP
         {
             try
             {
-                // Your image display logic goes here
-                // For simplicity, you can open a new window with an image viewer
                 ImageViewerWindow imageViewer = new ImageViewerWindow(imagePath);
                 imageViewer.Show();
             }
             catch (Exception ex)
             {
-                // Handle exceptions, e.g., file not found, invalid image format, etc.
                 MessageBox.Show($"Error displaying image: {ex.Message}", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
--- OscilloscopeWindow.xaml.cs
+++ OscilloscopeWindow.xaml.cs
@@ -1,84 +1,70 @@
-﻿// Inside the OscilloscopeWindow.xaml.cs
-
-using System;
+﻿using System;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
-
 namespace AVSP
 {
     public partial class OscilloscopeWindow : Window
     {
-        public OscilloscopeWindow(string imagePath)
-        {
-            // No need for InitializeComponent() when not using XAML-defined UI elements
+        private int redInterference;
+        private int greenInterference;
+        private int blueInterference;
 
-            // Load the image and convert it to an analog signal
+        public OscilloscopeWindow(string imagePath, int redInterference = 0, int greenInterference = 0, int blueInterference = 0)
+        {
+            InitializeComponent();
+
+            this.redInterference = redInterference;
+            this.greenInterference = greenInterference;
+            this.blueInterference = blueInterference;
+
             BitmapImage bitmap = new BitmapImage(new Uri(imagePath, UriKind.RelativeOrAbsolute));
             ConvertImageToAnalogSignal(bitmap);
         }
 
         private void ConvertImageToAnalogSignal(BitmapImage bitmap)
         {
-            // Create a Canvas to draw the analog signal
             var canvas = new Canvas();
             canvas.Width = bitmap.PixelWidth;
             canvas.Height = bitmap.PixelHeight;
 
-            // Add the Canvas to the OscilloscopeWindow
+            ApplyInterferenceSettings(bitmap);
             this.Content = canvas;
 
-            // Iterate through the image pixels
             for (int x = 0; x < bitmap.PixelWidth; x++)
             {
                 for (int y = 0; y < bitmap.PixelHeight; y++)
                 {
-                    // Get the color of the current pixel
                     Color pixelColor = GetPixelColor(bitmap, x, y);
-
-                    // Map the color value to a voltage level
                     double voltage = MapColorToVoltage(pixelColor);
-
-                    // Draw a point on the Canvas based on the voltage and time (x-coordinate)
                     DrawPoint(canvas, x, voltage);
                 }
             }
         }
 
-        private Color GetPixelColor(BitmapImage bitmap, int x, int y)
+        private void ApplyInterferenceSettings(BitmapImage bitmap)
         {
-            // Get the color of the specified pixel
-            CroppedBitmap cb = new CroppedBitmap(bitmap, new Int32Rect(x, y, 1, 1));
-            byte[] pixel = new byte[4];
-            cb.CopyPixels(pixel, 4, 0);
-
-            return Color.FromRgb(pixel[2], pixel[1], pixel[0]);
+            for (int x = 0; x < bitmap.PixelWidth; x++)
+            {
+                for (int y = 0; y < bitmap.PixelHeight; y++)
+                {
+                    Color originalColor = GetPixelColor(bitmap, x, y);
+                    Color interferedColor = ApplyInterference(originalColor);
+                    bitmap.SetPixel(x, y, interferedColor); // You might need to replace this line with an appropriate method based on your requirements
+                }
+            }
         }
 
-        private double MapColorToVoltage(Color color)
+        private Color ApplyInterference(Color originalColor)
         {
-            // Map the color to a voltage level (you might want to adjust the scaling)
-            double grayscaleValue = color.R * 0.3 + color.G * 0.59 + color.B * 0.11;
-            return grayscaleValue / 255.0 * 5.0; // Assuming a 5V range
-        }
+            int red = (originalColor.R + redInterference) % 256;
+            int green = (originalColor.G + greenInterference) % 256;
+            int blue = (originalColor.B + blueInterference) % 256;
 
-        private void DrawPoint(Canvas canvas, int x, double voltage)
-        {
-            // Draw a point on the Canvas based on the voltage and time (x-coordinate)
-            var point = new Ellipse();
-            point.Fill = Brushes.Red; // You can customize the color
-            point.Width = 2;
-            point.Height = 2;
-
-            // Adjust the y-coordinate based on the voltage (you might need to scale it)
-            Canvas.SetTop(point, voltage * canvas.Height);
-            Canvas.SetLeft(point, x);
-
-            // Add the point to the Canvas
-            canvas.Children.Add(point);
+            return Color.FromRgb((byte)red, (byte)green, (byte)blue);
         }
     }
 }
